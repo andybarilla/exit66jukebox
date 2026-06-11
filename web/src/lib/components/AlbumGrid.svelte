3c5ddd7c1493f3b90c4b659634c9c4f7d854508d
Andy Barilla <me@andybarilla.com>
2026-06-11 15:46:51 -0600

    feat(web): AlbumCard + AlbumGrid

---
+++ web/src/lib/components/AlbumGrid.svelte
@@ -0,0 +1,11 @@
+<script>
+  import AlbumCard from './AlbumCard.svelte';
+  let { cards = [], onOpen, onRequest } = $props();
+</script>
+<div style="display:grid; grid-template-columns:repeat(auto-fill, minmax(150px, 1fr)); gap:14px;">
+  {#each cards as a (a.id)}
+    <AlbumCard title={a.name} artist={a.artistName} meta={a.meta} initial={a.initial}
+      cover={a.cover} gradient={a.gradient}
+      onOpen={() => onOpen(a)} onRequest={() => onRequest(a)} />
+  {/each}
+</div>
